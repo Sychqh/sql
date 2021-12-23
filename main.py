cd76922ea4b93da00b4c440fe6a1177dde738559
Sychqh <sychqh@gmail.com>
2021-12-23 19:20:33 +0500

    Правки 4х заданий

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import sqlite3
 import pandas as pd
+import re
 
+def clear_tags(value):
+    return re.sub(r'<[^>]*>', '', str(value))
 
 def main():
     con = sqlite3.connect('works.sqlite')
@@ -17,8 +20,8 @@ def main():
                    'skills TEXT,'
                    'otherInfo TEXT)')
 
-    df = pd.read_csv("works.csv")
-    df.to_sql("works", con, if_exists='append', index=False)
+    data = pd.read_csv("works.csv")
+    data.to_sql("works", con, if_exists='append', index=False)
     con.commit()
 
     # Таблица с гендерами
@@ -27,6 +30,7 @@ def main():
     cursor.execute('INSERT INTO genders SELECT DISTINCT gender FROM works WHERE gender IS NOT NULL')
     con.commit()
     print(cursor.execute('SELECT * FROM genders').fetchall())
+    print()
 
     # Таблица с образованиями
     cursor.execute('DROP TABLE IF EXISTS educations')
@@ -34,13 +38,15 @@ def main():
     cursor.execute('INSERT INTO educations SELECT DISTINCT educationType FROM works WHERE works.educationType IS NOT NULL')
     con.commit()
     print(cursor.execute('SELECT * FROM educations').fetchall())
+    print()
 
     # Таблица с названиями работы
     cursor.execute('DROP TABLE IF EXISTS jobTitles')
     cursor.execute('CREATE TABLE jobTitles(jobTitle TEXT PRIMARY KEY )')
     cursor.execute('INSERT INTO jobTitles SELECT DISTINCT jobTitle FROM works WHERE jobTitle IS NOT NULL')
     con.commit()
-    # print(cursor.execute('SELECT * FROM jobTitles').fetchall())
+    print(cursor.execute('SELECT * FROM jobTitles').fetchall())
+    print()
 
     # Таблица с квалификациями
     cursor.execute('DROP TABLE IF EXISTS qualifications')
@@ -48,6 +54,8 @@ def main():
     cursor.execute('INSERT INTO qualifications SELECT DISTINCT qualification FROM works WHERE qualification IS NOT NULL')
     con.commit()
     print(cursor.execute('SELECT * FROM qualifications').fetchall())
+    print()
+
 
 if __name__ == '__main__':
     main()
